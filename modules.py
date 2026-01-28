3a4d79c9e19e12e993f63d6698fd0b128ee5f762
YY-258-bit <augie_gbyn@yahoo.com>
2026-01-28 21:21:03 +0800

    Update modules.py

--- modules.py
+++ modules.py
@@ -7,28 +7,6 @@ from ultralytics.nn.modules.block import C2f, DFL
 from ultralytics.utils.tal import dist2bbox, make_anchors
 from ultralytics.utils.ops import make_divisible
 
-def autopad(k, p=None, d=1):
-    if d > 1:
-        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]
-    if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]
-    return p
-
-try:
-    from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2d
-    from mmengine.model import constant_init, normal_init
-except ImportError:
-    def constant_init(module, val, bias=0):
-        if hasattr(module, 'weight') and module.weight is not None:
-            nn.init.constant_(module.weight, val)
-        if hasattr(module, 'bias') and module.bias is not None:
-            nn.init.constant_(module.bias, bias)
-    def normal_init(module, mean=0, std=1, bias=0):
-        if hasattr(module, 'weight') and module.weight is not None:
-            nn.init.normal_(module.weight, mean, std)
-        if hasattr(module, 'bias') and module.bias is not None:
-            nn.init.constant_(module.bias, bias)
-
 class PFE(nn.Module):
     def __init__(self, inc) -> None:
         super().__init__()
@@ -198,154 +176,6 @@ class DySample(nn.Module):
             return self.forward_pl(x)
         return self.forward_lp(x)
 
-class h_sigmoid(nn.Module):
-    def __init__(self, inplace=True, h_max=1):
-        super(h_sigmoid, self).__init__()
-        self.relu = nn.ReLU6(inplace=inplace)
-        self.h_max = h_max
-
-    def forward(self, x):
-        return self.relu(x + 3) * self.h_max / 6
-
-class DyReLU(nn.Module):
-    def __init__(self, inp, reduction=4, lambda_a=1.0, K2=True, use_bias=True, use_spatial=False,
-                 init_a=[1.0, 0.0], init_b=[0.0, 0.0]):
-        super(DyReLU, self).__init__()
-        self.oup = inp
-        self.lambda_a = lambda_a * 2
-        self.K2 = K2
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.use_bias = use_bias
-        if K2:
-            self.exp = 4 if use_bias else 2
-        else:
-            self.exp = 2 if use_bias else 1
-        self.init_a = init_a
-        self.init_b = init_b
-        if reduction == 4:
-            squeeze = inp // reduction
-        else:
-            squeeze = make_divisible(inp // reduction, 4)
-        self.fc = nn.Sequential(
-            nn.Linear(inp, squeeze),
-            nn.ReLU(inplace=True),
-            nn.Linear(squeeze, self.oup * self.exp),
-            h_sigmoid()
-        )
-        if use_spatial:
-            self.spa = nn.Sequential(
-                nn.Conv2d(inp, 1, kernel_size=1),
-                nn.BatchNorm2d(1),
-            )
-        else:
-            self.spa = None
-
-    def forward(self, x):
-        if isinstance(x, list):
-            x_in = x[0]
-            x_out = x[1]
-        else:
-            x_in = x
-            x_out = x
-        b, c, h, w = x_in.size()
-        y = self.avg_pool(x_in).view(b, c)
-        y = self.fc(y).view(b, self.oup * self.exp, 1, 1)
-        if self.exp == 4:
-            a1, b1, a2, b2 = torch.split(y, self.oup, dim=1)
-            a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]
-            a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
-            b1 = b1 - 0.5 + self.init_b[0]
-            b2 = b2 - 0.5 + self.init_b[1]
-            out = torch.max(x_out * a1 + b1, x_out * a2 + b2)
-        elif self.exp == 2:
-            if self.use_bias:
-                a1, b1 = torch.split(y, self.oup, dim=1)
-                a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]
-                b1 = b1 - 0.5 + self.init_b[0]
-                out = x_out * a1 + b1
-            else:
-                a1, a2 = torch.split(y, self.oup, dim=1)
-                a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]
-                a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
-                out = torch.max(x_out * a1, x_out * a2)
-        elif self.exp == 1:
-            a1 = y
-            a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]
-            out = x_out * a1
-        if self.spa:
-            ys = self.spa(x_in).view(b, -1)
-            ys = F.softmax(ys, dim=1).view(b, 1, h, w) * h * w
-            ys = F.hardtanh(ys, 0, 3, inplace=True)/3
-            out = out * ys
-        return out
-
-class DyDCNv2(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, norm_cfg=dict(type='GN', num_groups=16, requires_grad=True)):
-        super().__init__()
-        self.with_norm = norm_cfg is not None
-        bias = not self.with_norm
-        self.conv = ModulatedDeformConv2d(
-            in_channels, out_channels, 3, stride=stride, padding=1, bias=bias)
-        if self.with_norm:
-            if norm_cfg['type'] == 'GN':
-                self.norm = nn.GroupNorm(num_groups=norm_cfg['num_groups'], num_channels=out_channels)
-            elif norm_cfg['type'] == 'BN':
-                self.norm = nn.BatchNorm2d(out_channels)
-
-    def forward(self, x, offset, mask):
-        x = self.conv(x.contiguous(), offset, mask)
-        if self.with_norm:
-            x = self.norm(x)
-        return x
-
-class DyHeadBlock(nn.Module):
-    def __init__(self, in_channels, norm_type='GN', zero_init_offset=True, act_cfg=dict(type='HSigmoid', bias=3.0, divisor=6.0)):
-        super().__init__()
-        self.zero_init_offset = zero_init_offset
-        self.offset_and_mask_dim = 3 * 3 * 3
-        self.offset_dim = 2 * 3 * 3
-        norm_dict = dict(type='GN', num_groups=16, requires_grad=True) if norm_type == 'GN' else dict(type='BN', requires_grad=True)
-        self.spatial_conv_high = DyDCNv2(in_channels, in_channels, norm_cfg=norm_dict)
-        self.spatial_conv_mid = DyDCNv2(in_channels, in_channels)
-        self.spatial_conv_low = DyDCNv2(in_channels, in_channels, stride=2)
-        self.spatial_conv_offset = nn.Conv2d(in_channels, self.offset_and_mask_dim, 3, padding=1)
-        self.scale_attn_module = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1), nn.Conv2d(in_channels, 1, 1),
-            nn.ReLU(inplace=True), h_sigmoid())
-        self.task_attn_module = DyReLU(in_channels)
-        self._init_weights()
-
-    def _init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                normal_init(m, 0, 0.01)
-        if self.zero_init_offset:
-            constant_init(self.spatial_conv_offset, 0)
-
-    def forward(self, x):
-        outs = []
-        for level in range(len(x)):
-            offset_and_mask = self.spatial_conv_offset(x[level])
-            offset = offset_and_mask[:, :self.offset_dim, :, :]
-            mask = offset_and_mask[:, self.offset_dim:, :, :].sigmoid()
-            mid_feat = self.spatial_conv_mid(x[level], offset, mask)
-            sum_feat = mid_feat * self.scale_attn_module(mid_feat)
-            summed_levels = 1
-            if level > 0:
-                low_feat = self.spatial_conv_low(x[level - 1], offset, mask)
-                sum_feat += low_feat * self.scale_attn_module(low_feat)
-                summed_levels += 1
-            if level < len(x) - 1:
-                high_feat = F.interpolate(
-                    self.spatial_conv_high(x[level + 1], offset, mask),
-                    size=x[level].shape[-2:],
-                    mode='bilinear',
-                    align_corners=True)
-                sum_feat += high_feat * self.scale_attn_module(high_feat)
-                summed_levels += 1
-            outs.append(self.task_attn_module(sum_feat / summed_levels))
-        return outs
-
 class Detect_DyHead(nn.Module):
     dynamic = False
     export = False
@@ -393,4 +223,5 @@ class Detect_DyHead(nn.Module):
         m = self
         for a, b, s in zip(m.cv2, m.cv3, m.stride):
             a[-1].bias.data[:] = 1.0
+
             b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)
